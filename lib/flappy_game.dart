187a6bbe440d38df81163a71c4e894b60430dfc3
EstebanDSM <stephanedasil@gmail.com>
2021-11-11 15:22:15 +0100

    fullScreen / mode Portrait / initialize FlappyGame

---
+++ lib/flappy_game.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class FlappyGame extends Game {
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'package:flame/util.dart';
+import 'package:flappy_bird/flappy_game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  
+
+  Util flameUtil = Util();
+
+  //Forcer mode plein écran
+  flameUtil.fullScreen();
+
+  //Forcer mode portrait
+  flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  //Création d'une instance de la classe FlappyGame
+  FlappyGame game = FlappyGame();
+
+  runApp(game.widget);
+
+
 }
 
 
